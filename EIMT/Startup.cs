523e36424d85bc12636d9022b3a59f55cd2c75eb
doczir <doczi.r@gmail.com>
2014-11-01 17:33:26 +0100

    Initial commit

---
+++ EIMT/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EIMT.Startup))]
+namespace EIMT
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
