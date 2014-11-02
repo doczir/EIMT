0b314c0d0184ac863409788434e16cd212448710
doczir <doczi.r@gmail.com>
2014-11-02 17:41:52 +0100

    Added migration to database.

---
+++ EIMT/Models/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace EIMT.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
