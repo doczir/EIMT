baace517912547ba67052e13d6db18ab189bf959
doczir <doczi.r@gmail.com>
2014-11-02 23:40:11 +0100

    Service Providers

---
+++ EIMT/Models/ServiceProviderModels.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EIMT.Models
+{
+    public class ServiceProvider
+    {
+
+        [Key]
+        public string Id { get; set; }
+        public int Name { get; set; }
+        public string AccountNumber { get; set; }
+        public string Password { get; set; }
+    }
+}
